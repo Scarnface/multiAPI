543a696305866b63c5ed2737e689d383ea229f76
Scarnface <marcclare@gmail.com>
2020-08-08 18:40:15 +0100

    Dynamic parsing added

--- libs/js/scripts.js
+++ libs/js/scripts.js
@@ -1,145 +1,75 @@
-$("#api1").click(function () {
+$("#api1, #api2, #api3, #api4, #api5").click(function () {
+  //Pulls the apiNum variable from the button triggering the click event
   var button = this.id;
 
-  $.ajax({
-    url: "libs/php/functions.php",
-    type: "POST",
-    dataType: "json",
-    data: {
+  //Sets the data to send depending on the API used.
+  if (button === "api1") {
+    var dataToSend = {
       apiNum: "api1",
       country: $("#selCountry").val(),
-    },
-    success: function (result) {
-      console.log(result);
-
-      if (result.status.name == "ok") {
-        if (button === "api1") {
-          $("#results").find("tbody").empty();
-          $.each(result, function (key, value) {
-            $.each(value[0], function (k, val) {
-              $("#results tbody").append(
-                "<tr><td>" + k + "</td><td>" + val + "</td></tr>"
-              );
-            });
-          });
-        }
-      }
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      console.log(jqXHR, textStatus, errorThrown);
-    },
-  });
-});
-
-$("#api2").click(function () {
-  $.ajax({
-    url: "libs/php/functions.php",
-    type: "POST",
-    dataType: "json",
-    data: {
+    };
+  } else if (button === "api2") {
+    var dataToSend = {
       apiNum: "api2",
       lat: $("#selLat").val(),
       lng: $("#selLng").val(),
-    },
-    success: function (result) {
-      console.log(result);
-
-      if (result.status.name == "ok") {
-        $("#results1, #results3, #results4, #results5").hide();
-        $("#results2").show();
-        $("#r2Toponym").html(result["data"][0]["toponymName"]);
-        $("#r2CountryCode").html(result["data"][0]["countryCode"]);
-        $("#r2Type").html(result["data"][0]["fcodeName"]);
-      }
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      console.log(jqXHR, textStatus, errorThrown);
-    },
-  });
-});
-
-$("#api3").click(function () {
-  $.ajax({
-    url: "libs/php/functions.php",
-    type: "POST",
-    dataType: "json",
-    data: {
+    };
+  } else if (button === "api3") {
+    var dataToSend = {
       apiNum: "api3",
       lat: $("#selLat").val(),
       lng: $("#selLng").val(),
-    },
-    success: function (result) {
-      console.log(result);
-
-      if (result.status.name == "ok") {
-        $("#results1, #results2, #results4, #results5").hide();
-        $("#results3").show();
-        $("#r3DateTime").html(result["data"]["datetime"]);
-        $("#r3Clouds").html(result["data"]["clouds"]);
-        $("#r3Humidity").html(result["data"]["humidity"]);
-        $("#r3Temperature").html(result["data"]["temperature"]);
-      }
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      console.log(jqXHR, textStatus, errorThrown);
-    },
-  });
-});
-
-$("#api4").click(function () {
-  $.ajax({
-    url: "libs/php/functions.php",
-    type: "POST",
-    dataType: "json",
-    data: {
-      apiNum: "api4",
-      q: $("#selLocation").val(),
-    },
-    success: function (result) {
-      console.log(result);
-
-      if (result.status.name == "ok") {
-        $("#results1, #results2, #results3, #results5").hide();
-        $("#results4").show();
-        $("#r4TitleA").html(result["data"][0]["title"]);
-        $("#r4SummaryA").html(result["data"][0]["summary"]);
-        $("#r4UrlA").html(result["data"][0]["wikipediaUrl"]);
-        $("#r4TitleB").html(result["data"][1]["title"]);
-        $("#r4SummaryB").html(result["data"][1]["summary"]);
-        $("#r4UrlB").html(result["data"][1]["wikipediaUrl"]);
-        $("#r4TitleC").html(result["data"][2]["title"]);
-        $("#r4SummaryC").html(result["data"][2]["summary"]);
-        $("#r4UrlC").html(result["data"][2]["wikipediaUrl"]);
-      }
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      console.log(jqXHR, textStatus, errorThrown);
-    },
-  });
-});
-
-$("#api5").click(function () {
-  $.ajax({
-    url: "libs/php/functions.php",
-    type: "POST",
-    dataType: "json",
-    data: {
+    };
+  } else if (button === "api4") {
+    var dataToSend = { apiNum: "api4", q: $("#selLocation").val() };
+  } else if (button === "api5") {
+    var dataToSend = {
       apiNum: "api5",
       lat: $("#selLat").val(),
       lng: $("#selLng").val(),
-    },
+    };
+  }
+
+  $.ajax({
+    url: "libs/php/functions.php",
+    type: "POST",
+    dataType: "json",
+    data: dataToSend,
     success: function (result) {
       console.log(result);
-
       if (result.status.name == "ok") {
-        $("#results1, #results2, #results3, #results4").hide();
-        $("#results5").show();
-        $("#r5PlaceA").html(result["data"][0]["placeName"]);
-        $("#r5PostCodeA").html(result["data"][0]["postalCode"]);
-        $("#r5PlaceB").html(result["data"][1]["placeName"]);
-        $("#r5PostCodeB").html(result["data"][1]["postalCode"]);
-        $("#r5PlaceC").html(result["data"][2]["placeName"]);
-        $("#r5PostCodeC").html(result["data"][2]["postalCode"]);
+        //Removes any existing table
+        $("#results").find("tbody").empty();
+
+        //Loops the JSON depending on whether it contains an array or object
+        $.each(result, function (key, value) {
+          if (Array.isArray(value)) {
+            for (var i = 0; i < value.length; i++) {
+              $.each(value[i], JSONIterator);
+            }
+          } else {
+            $.each(value, JSONIterator);
+          }
+        });
+
+        function JSONIterator(key, value) {
+          if (value != null && typeof value == "object") {
+            // Recurse into children
+            $.each(value, JSONIterator);
+          } else {
+            //Removes request info from JSON and adds to results table
+            if (
+              key !== "code" &&
+              key !== "name" &&
+              key !== "description" &&
+              key !== "returnedIn"
+            ) {
+              $("#results tbody").append(
+                "<tr><td>" + key + "</td><td>" + value + "</td></tr>"
+              );
+            }
+          }
+        }
       }
     },
     error: function (jqXHR, textStatus, errorThrown) {
--- libs/php/functions.php
+++ libs/php/functions.php
@@ -1,55 +1,45 @@
 <?php
 
-	$executionStartTime = microtime(true) / 1000;
+  $executionStartTime = microtime(true) / 1000;
+  
+  //Sets query fields applicable to all APIs
+  $query_fields_default = [
+    'formatted' => 'true',
+    'lang' => 'en',
+    'username' => 'scarnface',
+    //Comment out the line below for simpler reponses
+    'style' => 'full',
+  ];
 
+  // Detects the click event and then completes the query fields based on the API requested
   if(isset($_POST['apiNum'])) {
     if($_POST['apiNum'] == 'api1') {
       $url = 'http://api.geonames.org/countryInfoJSON';
-      $query_fields = [
-        'formatted' => 'true',
-        'lang' => 'en',
-        'username' => 'scarnface',
-        'style' => 'full',
+      $query_fields = $query_fields_default + [
         'country' => $_REQUEST['country']
       ];
     } elseif($_POST['apiNum'] == 'api2') {
       $url = 'http://api.geonames.org/findNearbyPlaceNameJSON';
-      $query_fields = [
-        'formatted' => 'true',
-        'lang' => 'en',
-        'username' => 'scarnface',
-        'style' => 'full',
+      $query_fields = $query_fields_default + [
         'lat' => $_REQUEST['lat'],
         'lng' => $_REQUEST['lng'],             
       ];
     } elseif($_POST['apiNum'] == 'api3') {
       $url = 'http://api.geonames.org/findNearByWeatherJSON';
-      $query_fields = [
-        'formatted' => 'true',
-        'lang' => 'en',
-        'username' => 'scarnface',
-        'style' => 'full',  
+      $query_fields = $query_fields_default + [ 
         'lat' => $_REQUEST['lat'],
         'lng' => $_REQUEST['lng'],  
       ];
     } elseif($_POST['apiNum'] == 'api4') {
       $url = 'http://api.geonames.org/wikipediaSearchJSON';
-      $query_fields = [
-        'formatted' => 'true',
-        'lang' => 'en',
-        'username' => 'scarnface',
-        'style' => 'full', 
+      $query_fields = $query_fields_default + [ 
         'maxRows' => 3, 
         'q' => $_REQUEST['q'],
       ];
     } elseif($_POST['apiNum'] == 'api5') {
       $url = 'http://api.geonames.org/findNearbyPostalCodesJSON';
-      $query_fields = [
-        'formatted' => 'true',
-        'lang' => 'en',
-        'username' => 'scarnface',
-        'radius' => 10,
-        'style' => 'full', 
+      $query_fields = $query_fields_default + [
+        'radius' => 10, 
         'maxRows' => 3, 
         'lat' => $_REQUEST['lat'],
         'lng' => $_REQUEST['lng'],
@@ -62,10 +52,12 @@
   $response = json_decode(curl_exec($curl), true);
   curl_close($curl);	
 
+  //Appends request info to result
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "mission saved";
   $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+  //Detects the click event and sets the correct key based on the API requested
   if(isset($_POST['apiNum'])) {
     if($_POST['apiNum'] == 'api1' or $_POST['apiNum'] == 'api2' or $_POST['apiNum'] == 'api4') {
       $output['data'] = $response['geonames'];
